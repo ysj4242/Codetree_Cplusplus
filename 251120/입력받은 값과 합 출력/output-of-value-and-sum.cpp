97f8f7b76058a674970736ada0ec0cfb10387b3f
ysj4242 <110522265+ysj4242@users.noreply.github.com>
2025-11-20 00:49:03 +0900

    Title: 입력받은 값과 합 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A;
+    int B;
+    cin >> A >> B;
+    cout << A << " " << B << " " << A+B;
+    
+    
+    return 0;
+}
