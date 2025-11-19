eb2afa2b1b7e4adfc6edc4b386c740506bfd57e4
ysj4242 <110522265+ysj4242@users.noreply.github.com>
2025-11-20 00:44:20 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a;
+    cout << "\n";
+    cout << b;
+    cout << "\n";
+    cout << c;
+    return 0;
+}
