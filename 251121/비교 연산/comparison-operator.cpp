15beac84661393183b985d6a5b2bcbbabc134a4e
ysj4242 <110522265+ysj4242@users.noreply.github.com>
2025-11-21 01:04:35 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251121/비교 연산/comparison-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+     cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a < b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    return 0;
+}
