3f128d6b5d8440ea4989a8040fb8807657b3c23a
ysj4242 <110522265+ysj4242@users.noreply.github.com>
2025-11-21 01:29:25 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251121/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    if (N >= 3000)
+    cout << "book";
+    else if (N < 1000)
+    cout << "no";
+    else
+    cout << "mask";
+    return 0;
+}
