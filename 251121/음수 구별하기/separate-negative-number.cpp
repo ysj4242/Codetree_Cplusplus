2347026584c126786280066db3648dec1f49c74a
ysj4242 <110522265+ysj4242@users.noreply.github.com>
2025-11-21 00:58:42 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251121/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    cout << N << endl;
+    if (N < 0){
+        cout << "minus";
+    }
+    return 0;
+}
